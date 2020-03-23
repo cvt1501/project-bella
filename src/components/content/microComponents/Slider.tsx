b7bf8e77852803f6eba31df3c2dd12408f6787d2
cvt1501 <kevin_cavenatti@hotmail.com>
2020-03-23 15:39:38 -0300

    Slider Feito

--- src/components/content/microComponents/Slider.tsx
+++ src/components/content/microComponents/Slider.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
-export default () => {
+export default (): any => {
     return (
         <div className='ikc__full-banner-container'>
             <div className='ikc__banner-box'>
                     <a>
+                        <img src='./../../../assets/images/ikc__slider01.jpg' title='Banner Principal'/>
                     </a>
             </div>
         </div>
